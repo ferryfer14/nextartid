643a4186a0427c9a05ea2075589e1584a3edfa8b
ferryfer14 <profdrachmadferry@gmail.com>
2023-01-17 21:02:59 +0700

    invo

--- app/Http/Controllers/Frontend/ArtistManagementController.php
+++ app/Http/Controllers/Frontend/ArtistManagementController.php
@@ -1584,7 +1584,7 @@ class ArtistManagementController extends Controller
         $transaction = Transaction::withoutGlobalScopes()->where('id', $this->request->route('id'))->first();
         $pdf = PDF::loadview('artist-management.invoice', ['transaction' => $transaction]);
         $pdf->setPaper('a4', 'portrait');
-        return $pdf->download('Invoice-'.$transaction->transaction_id);
+        return $pdf->download('Invoice-'.$transaction->transaction_id.'.pdf');
     }
 
     public function editPatner()
